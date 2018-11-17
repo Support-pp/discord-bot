5783febc8c8d542a8c06180793719930801dd539
Johannes Waigel <support@allesverhext.de>
2018-11-17 20:24:56 +0100

    Create serverId.go

---
+++ commands/serverId.go
@@ -0,0 +1,23 @@
+package commands
+
+import "github.com/bwmarrin/discordgo"
+
+/*
+	This is the serverid command.
+	Response: embed :: info
+	Permission: all
+ */
+
+func CMD_ch(s *discordgo.Session, m *discordgo.MessageCreate)  {
+
+	cmdType := "ServerId"
+
+	embed := &discordgo.MessageEmbed{
+
+		Color:       0x4F545C, // Grey
+		Title:     "<:support:386227216159211531> **"+cmdType+"** | Support++",
+		Description: "Du kannst die ServerId ganz einfach heruasfinden. Discord hat eine Schritt-für-Schritt Anleitung erstellt. https://support.discordapp.com/hc/de/articles/206346498-Wie-finde-ich-meine-Server-ID-",
+	}
+	s.ChannelMessageSendEmbed(m.ChannelID, embed)
+
+}
